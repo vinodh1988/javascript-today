d0c7ecf5ff99b2566fb13d6908cacbb51106aa94
vinodhleo <vinodhleo@outlook.com>
2022-03-28 14:29:09 +0530

    array-ref

---
+++ arraymore.js
@@ -0,0 +1,20 @@
+a=[5,353,45,64,674,5,"arjun",[34,63,645], [4,35,[32,42,"Ramesh",[34,53,"Tim"]]]]
+
+b=a
+
+//is this syntactically correct
+
+// what console statement should be write to print ramesh alone on the screen
+
+console.log(a[8][2][2])
+console.log(a[8][2][3][2])
+console.log(a)
+b[3]=[1,2,3]
+console.log(a)
+
+c=a[7]
+c[3]="Rahul"
+
+console.log(c)
+console.log(a)
+
