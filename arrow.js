9a5b8c29d3c1df2caedb3fbcff4db6036b237e8f
vinodhleo <vinodhleo@outlook.com>
2022-03-28 16:12:38 +0530

    functions

---
+++ arrow.js
@@ -0,0 +1,30 @@
+ add=function(a,b){
+    return a+b
+}  
+
+console.log(add(123,34))
+
+ add = function(a,b,c){
+    return a+b+c
+}
+
+console.log(add(4,5,334))
+
+function hello(){
+    console.log("HELLO")
+}
+
+
+
+hello()
+
+function hello(name){
+    console.log("Hi!! ",name)
+}
+
+
+hello("Rajeev")
+
+p=hello
+
+p("Ram")
---
+++ function3.js
@@ -0,0 +1,20 @@
+let p=[x,y,z,function(){
+    console.log("cool")
+}]
+
+function x(){
+    console.log("Hello")
+}
+
+function y(a)
+{
+    console.log("Hello ", a)
+}
+
+function z(){
+    console.log("Hi", c)
+}
+
+p[0]()
+p[1]("India")
+p[3]()
